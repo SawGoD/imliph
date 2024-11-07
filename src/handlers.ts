9c33bfa5660d75387a3fe71285c6ab6a3c1dc4ce
SawGoD <flay.my.number.43@mail.ru>
2024-11-07 14:38:00 +0300

    feat: added handler for download any favicon

--- src/handlers.ts
+++ src/handlers.ts
@@ -25,6 +25,39 @@ const sendGoodUrlMessage = async (ctx: any, message: string, imgLink: string, im
 }
 
 /**
+ * Downloads the favicon from the specified URL.
+ *
+ * @param domain - The domain to fetch the favicon for.
+ * @returns {Promise<Buffer>} - A promise that resolves to the favicon as a Buffer.
+ */
+const downloadFavicon = async (domain: string, size: number): Promise<{ faviconBuffer: Buffer; title: string }> => {
+    const url = `https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${domain}&size=${size}`
+    const response = await axios.get(url, { responseType: 'arraybuffer' })
+    const faviconBuffer = Buffer.from(response.data)
+    const title = domain.split('/')[2] || 'Без названия'
+    return { faviconBuffer, title }
+}
+
+const tryDownloadFavicon = async (url: string) => {
+    const sizes = [256, 128, 64, 32, 16]
+    const requests = sizes.map((size) =>
+        downloadFavicon(url, size).catch(() => {
+            console.error(`Не удалось скачать с размером: ${size}`)
+            return undefined
+        })
+    )
+    const results = await Promise.all(requests)
+    return results.find((result) => result !== undefined)
+}
+
+const sendFaviconFile = async (ctx: CTX, url: string) => {
+    const faviconBuffer = await tryDownloadFavicon(url)
+    faviconBuffer
+        ? await ctx.replyWithDocument({ source: faviconBuffer.faviconBuffer, filename: `${faviconBuffer.title}.ico` })
+        : await ctx.reply('Не удалось скачать')
+}
+
+/**
  * Handles all types of messages by running the appropriate function for each one.
  *
  * @param ctx - The Telegraf context.
@@ -49,3 +82,15 @@ export const processImage = (ctx: CTX, fileId: string) =>
         .then((fileBuffer) => uploadToImgBB(fileBuffer))
         .then((uploadData) => sendGoodUrlMessage(ctx, msg.goodUpload(uploadData.url), uploadData.url, uploadData.delete_url))
         .then(() => ctx.deleteMessage())
+
+export const findUrl = async (ctx: CTX) => {
+    const text = ctx.message.text
+    const regex = /(https?:\/\/[^\s]+)/g
+    const url = text.match(regex)
+    if (url) {
+        sendFaviconFile(ctx, url[0])
+        // ctx.reply(url, { reply_to_message_id: ctx.message.message_id })
+    } else {
+        ctx.reply('No links', { reply_to_message_id: ctx.message.message_id })
+    }
+}
