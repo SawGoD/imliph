1133c5f381806a589fa86c719f8e6b94212a8766
SawGoD <flay.my.number.43@mail.ru>
2024-11-07 22:19:39 +0300

    feat: minor changes in validates.ts

--- src/helpers/validates.ts
+++ src/helpers/validates.ts
@@ -8,7 +8,7 @@ import { messagesReplies as msg } from '../definitions/constants'
  * @returns {boolean} - Возвращает true, если расширение допустимо, иначе false.
  */
 export const validateExtension = (fileName: string, ctx: any): boolean => {
-    const isValid = ['.jpeg', '.jpg', '.png'].includes(fileName.slice(-4).toLowerCase())
+    const isValid = ['.jpeg', '.jpg', '.png', '.ico'].includes(fileName.slice(-4).toLowerCase())
     const message = msg.badDocExtension + '\n' + msg.desc.supDocExt
     !isValid && ctx.replyWithMarkdownV2(validateMessage(message), { reply_to_message_id: ctx.message.message_id })
     return isValid
@@ -20,4 +20,4 @@ export const validateExtension = (fileName: string, ctx: any): boolean => {
  * @param message - Сообщение для проверки.
  * @returns - Сообщение с экранированными символами.
  */
-export const validateMessage = (message: string) => message.replace(/[!.-]/g, '\\$&')
+export const validateMessage = (message: string) => message.replace(/[!.()-]/g, '\\$&')
